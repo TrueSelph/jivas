82de0646aefa2b169073abd26ecaccb2a71c57dc
Eldon Marks <eldon.marks@gmail.com>
2025-04-08 14:49:30 -0400

    Updated data param in interact walker to accept a list of data items; added serialization to dump_yaml function

--- jivas/agent/modules/agentlib/utils.py
+++ jivas/agent/modules/agentlib/utils.py
@@ -86,9 +86,22 @@ class Utils:
         return daf_root_path
 
     @staticmethod
+    def serialize_object(obj: Any) -> Any:
+        """Convert objects to dictionaries if needed."""
+        if isinstance(obj, dict):
+            return {k: Utils.serialize_object(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [Utils.serialize_object(i) for i in obj]
+        elif hasattr(obj, "__dict__"):
+            return obj.__dict__
+        else:
+            return obj
+
+    @staticmethod
     def dump_yaml_file(file_path: str, data: dict) -> None:
         """Dump data to a YAML file."""
         try:
+            data = Utils.serialize_object(data)
             yaml_output = yaml.dump(
                 data,
                 Dumper=LongStringDumper,
