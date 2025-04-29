58d5fc1183a8813687e0b26fbd48dbefcc4cf8c3
Eldon Marks <eldon.marks@gmail.com>
2025-04-29 11:36:28 -0400

    updated action installation routine; fixed action context overwriting and descriptor ordering bugs

--- jivas/__init__.py
+++ jivas/__init__.py
@@ -4,4 +4,4 @@ jivas package initialization.
 JIVAS is an Agentic Framework for rapidly prototyping and deploying graph-based, AI solutions.
 """
 
-__version__ = "2.0.0-alpha.36"
+__version__ = "2.0.0-alpha.37"
