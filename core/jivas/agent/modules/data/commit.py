525d109812601798663af0973f26383940cd7fbf
Eldon Marks <eldon.marks@gmail.com>
2025-08-17 07:22:13 -0400

    fixed linting errors

--- core/jivas/agent/modules/data/commit.py
+++ core/jivas/agent/modules/data/commit.py
@@ -1,9 +1,10 @@
 """Commit utilities for Jac memory."""
 
-from jac_cloud.plugin.jaseci import JacPlugin as Jac
 from jac_cloud.core.archetype import BaseAnchor, BulkWrite
 from jac_cloud.jaseci.datasources import Collection
+from jac_cloud.plugin.jaseci import JacPlugin as Jac
 from jaclang.runtimelib.constructs import Archetype
+
 
 def commit(anchor: BaseAnchor | None = None) -> None:
     """Commit all data from memory to datasource."""
